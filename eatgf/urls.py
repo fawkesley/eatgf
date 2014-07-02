a09d2c28cbbe9bc5fab121187b85334d38c5c48c
Paul M Furley <paul@paulfurley.com>
2014-07-02 21:12:00 +0100

    Make it more Liverpool-centric and compelling
    - Change the structure so it's all about Liverpool.
    - Put Liverpool in the navbar
    - Add a b&w background theme of the three graces
    - Change the styling a bit so the navbar is black and white
    - Added some call-to-actions to contribute restaurant reviews

--- eatgf/urls.py
+++ eatgf/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
 from eatgf.apps.locations.views import LocationList, LocationDetail
@@ -16,7 +17,9 @@ urlpatterns = [
     # url(r'^(?P<location>(' + SLUG_RE + ')/$',
     #    RedirectView.as_view(url=reverse('location-detail'), permanent=True))
 
-    url(r'^$',
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+
+    url(r'^locations/$',
         LocationList.as_view(),
         name='location-list'),
 
